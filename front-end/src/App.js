cbc95e70d0f371d7f47eca240ccb47c24b43e777
Ananda Indra Tan <anandaindratan45@gmail.com>
2024-08-23 21:58:22 +0800

    Initiate Tailwind and Styled-components setup in front-end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Testing = styled.div``
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Testing className="App">
+      <h1 className="font-bold">React App</h1>
+    </Testing>
   );
 }
 
