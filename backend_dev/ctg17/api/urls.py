78ad2385375d8fc60c421d4a52c5bf968d873aaf
Karan <thekaranvs@gmail.com>
2024-08-24 00:10:11 +0800

    add registration for admin

--- backend_dev/ctg17/api/urls.py
+++ backend_dev/ctg17/api/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('hello/', views.say_hello),
     path('user/register', views.register_user, name="register-user"),
+    path('staffuser/register', views.create_staffuser, name="register-staffuser"),
     path('events/', views.get_all_events, name="event-list"),
     path('events/<int:event_id>/', views.get_event_details, name="event-detail"),
     path('events/create/', views.create_event, name="create-event"),
