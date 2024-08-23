b1c61f1bcdfe30d00b2bc8892c56b636fd59a7f8
Karina Nathalie <karinanathalie2@gmail.com>
2024-08-23 22:22:41 +0800

    make migration for profile and application

---
+++ backend_dev/ctg17/api/constants.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+class RoleType(Enum):
+    STAFF = 'staff'
+    VOLUNTEER = 'volunteer'
+    PARTICIPANT = 'participant'
+
+    @classmethod
+    def choices(cls):
+        return [(key.name, key.value) for key in cls]
+
+class Gender(Enum):
+    MALE = 'male'
+    FEMALE = 'female'
+    
+    @classmethod
+    def choices(cls):
+        return [(key.name, key.value) for key in cls]
--- backend_dev/ctg17/api/models.py
+++ backend_dev/ctg17/api/models.py
@@ -1,5 +1,55 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import User
+from api.constants import Gender, RoleType
+
+class Profile(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+    name = models.CharField(max_length=225)
+    age = models.PositiveIntegerField()
+    gender = models.TextField(
+        choices=Gender.choices(),
+    )
+    role_type = models.TextField(
+        choices=RoleType.choices(),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} ({self.user.username})"
+    
+
+class Application(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+    user_profile = models.ForeignKey(
+        'Profile',
+        on_delete=models.PROTECT,
+        related_name='applications',
+        null=True, 
+        blank=True,
+    )
+    event = models.ForeignKey(
+        'Event',
+        on_delete=models.CASCADE,
+        related_name='applications',
+        null=True, 
+        blank=True,
+    )
+    role_type = models.TextField(
+        choices=RoleType.choices()
+    )
+
+    def __str__(self):
+        return f"{self.user_profile.name} applied as {self.role_type} for {self.event.event_name}"
+
 
 class Event(models.Model):
     # Basic event details
@@ -19,6 +69,12 @@ class Event(models.Model):
     deadline = models.DateTimeField()
     registered_participants = models.ManyToManyField(User, related_name="registered_participants", null=True, blank=True)
     registered_volunteers = models.ManyToManyField(User, related_name="registered_volunteers", null=True, blank=True)
+    staff = models.ManyToManyField(
+        'Profile', 
+        through='Application', 
+        related_name='managed_events',
+        limit_choices_to={'role_type': RoleType.STAFF.value}
+    )
 
     def __str__(self):
         return self.event_name
