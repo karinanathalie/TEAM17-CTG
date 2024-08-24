a1b0584d078a3e8949ff5ede638eeda6a2a073f7
Saidbek Khalikov <skhalikov2@gmail.com>
2024-08-24 12:10:03 +0800

    Fixed the sizing issue

--- front-end/src/Components/SideBar.js
+++ front-end/src/Components/SideBar.js
@@ -9,39 +9,41 @@ import PermIdentityOutlinedIcon from '@mui/icons-material/PermIdentityOutlined';
 export default function SideBar({ children }) {
 
     return (
-      <div className='flex flex-row'>
-        <div className=" mx-[30px] mt-[40px] sidebar w-[350px] h-full bg-white" style={{ width: '400px' }}>
-        <div className='my-[5px]'>
-            <img src='http://placehold.it/250x80'></img>
-        </div>
-          <nav>
-            <ul>
-              <li className='my-[7x] font-[700] py-[5px]'>
-                <HomeOutlinedIcon />
-                <NavLink to="/home" className="ml-[15px]" activeClassName="active-link">
-                  Home
-                </NavLink>
-              </li>
-              <li className='my-[7x] font-[700] py-[5px]'>
-                <BarChartOutlinedIcon />
-                <NavLink to="/events" className="ml-[15px]" activeClassName="active-link">
-                  Events
-                </NavLink>
-              </li>
-              <li className='my-[7x] font-[700] py-[5px]'>
-                <InfoOutlinedIcon />
-                <NavLink to="/about" className="ml-[15px]" activeClassName="active-link">
-                  About
-                </NavLink>
-              </li>
-              <li className='my-[7x] font-[700] py-[5px]'>
-                <PermIdentityOutlinedIcon />
-                <NavLink to="/user" className="ml-[15px]" activeClassName="active-link">
-                  User
-                </NavLink>
-              </li>
-            </ul>
-          </nav>
+      <div className='flex flex-row h-full w-full'>
+        <div className='bg-black rounded h-full'>
+            <div className=" mx-[30px] mt-[40px] sidebar w-[250px] h-full bg-black text-white">
+            <div className='my-[5px]'>
+                <img src='http://placehold.it/250x80'></img>
+            </div>
+            <nav>
+                <ul>
+                <li className='my-[7x] font-[700] py-[5px]'>
+                    <HomeOutlinedIcon />
+                    <NavLink to="/home" className="ml-[15px]" activeClassName="active-link">
+                    Home
+                    </NavLink>
+                </li>
+                <li className='my-[7x] font-[700] py-[5px]'>
+                    <BarChartOutlinedIcon />
+                    <NavLink to="/events" className="ml-[15px]" activeClassName="active-link">
+                    Events
+                    </NavLink>
+                </li>
+                <li className='my-[7x] font-[700] py-[5px]'>
+                    <InfoOutlinedIcon />
+                    <NavLink to="/about" className="ml-[15px]" activeClassName="active-link">
+                    About
+                    </NavLink>
+                </li>
+                <li className='my-[7x] font-[700] py-[5px]'>
+                    <PermIdentityOutlinedIcon />
+                    <NavLink to="/user" className="ml-[15px]" activeClassName="active-link">
+                    User
+                    </NavLink>
+                </li>
+                </ul>
+            </nav>
+            </div>
         </div>
         <div className='w-full h-full'>
           {children}
