e241978222cefd63ee96f05e395987a26d9e817a
Ananda Indra Tan <anandaindratan45@gmail.com>
2024-08-25 01:43:33 +0800

    EventDetails init

--- front-end/src/Components/Cards.js
+++ front-end/src/Components/Cards.js
@@ -3,7 +3,7 @@ import { ButtonFull, ButtonFull2 } from "./Button";
 import { CiCalendarDate , CiLocationOn } from "react-icons/ci";
 
 
-export const ScheduleCard = ({ onClick, date, eventName, eventSummary}) => {
+export const ScheduleCard = ({date, eventName, eventSummary}) => {
     
     const SplittedDate = date.split("-");
     const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
@@ -37,7 +37,7 @@ export const ScheduleCard = ({ onClick, date, eventName, eventSummary}) => {
   };
 
 
-  export const CarousellCard = ({ onClick, date, location, eventPicture, eventName}) => {
+  export const CarousellCard = ({date, location, eventPicture, eventName}) => {
     const SplittedDate = date.split("-");
     const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
     const monthIndex = parseInt(SplittedDate[1], 10) - 1;
@@ -83,7 +83,45 @@ export const ScheduleCard = ({ onClick, date, eventName, eventSummary}) => {
                 </ButtonFull>
             </div>
         </div>
-
     );
-
   };
+
+
+export const EventCard = ({date, location, eventPicture, eventName, eventSummary, role}) => {
+    const SplittedDate = date.split("-");
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const monthIndex = parseInt(SplittedDate[1], 10) - 1;
+    const month = months[monthIndex];
+    const day = SplittedDate[0];
+
+
+    return(
+        <div className="m-3 flex space-x- w-[1040px] h-[231px] rounded-[8px] px-[24px] py-[16px] bg-[#F2F2F2]">
+            <div className="flex flex-col justify-center w-[344px] h-[197px] rounded-[8px] bg-gray">
+                <img
+                    src={eventPicture}
+                    alt="event"
+                    className="w-full h-full object-cover rounded-t-[16px]"
+                />
+            </div>
+            <div className="flex flex-col pl-[41px]">
+                <div className="h-[150px] w-[600px]">
+                    <div className="font-semibold text-[24px]">{eventName}</div>
+                    <div className="pt-[1px] font-medium text-[14px] text-gray">{eventSummary}</div>
+                    <div className="pt-[1px] flex font-medium text-[14px] text-gray">
+                        <div className="flex flex-col justify-center">  
+                            <CiLocationOn />
+                        </div>
+                        <div className="pl-[12px]">
+                            {location}
+                        </div>
+                    </div>
+                </div>
+                <div className="flex justify-end pt-[37px]">
+                    <ButtonFull>Details</ButtonFull>
+                </div>
+            </div>
+           
+        </div>
+    );
+  }
--- front-end/src/Pages/EventDetails.js
+++ front-end/src/Pages/EventDetails.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { styled } from "styled-components";
+import SideBar from "../Components/SideBar";
 
 
 const Container = styled.div``;
@@ -8,8 +9,8 @@ const Container = styled.div``;
 export default function EventDetails(){
 
     return(
-        <Container>
-            
+        <Container className="flex">
+            <SideBar />
         </Container>
     )
 }
