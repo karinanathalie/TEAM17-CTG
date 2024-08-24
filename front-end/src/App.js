9766af8904c785240f907f35966ff715fab18468
vanessalaurel <vlaurel04@gmail.com>
2024-08-24 20:41:14 +0800

    new

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,34 +7,17 @@ import {
   Switch,
   NavLink,
 } from "react-router-dom";
-import { Home, Person, School, EmojiEvents } from "@mui/icons-material";
+// import { Home, Person, School, EmojiEvents } from "@mui/icons-material";
 import HomeView from "./Pages/Home.js";
 import SideBar from "./Components/SideBar.js";
 import styled from "styled-components";
-import { ButtonFull, ButtonFull2 } from "./Components/Button.js";
-import Login from "./Components/login2.jsx";
+
+import Login from "./Components/login.jsx";
 import Calendarr from "./Components/Calendar.jsx";
+import { Container } from "@mui/material";
 
 function App() {
-  return (
-    <Container>
-      <Calendarr></Calendarr>
-
-      <Router>
-        <SideBar>
-          <Switch>
-            <Route exact path="/" component={HomeView} />
-            <Route path="/about" component={HomeView} />
-            <Route path="/profile" component={HomeView} />
-            <Route path="/training" component={HomeView} />
-            <Route path="/volunteering" component={HomeView} />
-          </Switch>
-        </SideBar>
-      </Router>
-
-      {/* <ScheduleCard date="12-07-2003" eventName="Hello World" eventSummary="hello" /> */}
-    </Container>
-  );
+  return <Container>test</Container>;
 }
 
 export default App;
--- front-end/src/Components/SideBar.js
+++ front-end/src/Components/SideBar.js
@@ -1,78 +1,129 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-// import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import BarChartOutlinedIcon from '@mui/icons-material/BarChartOutlined';
-import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
-import PermIdentityOutlinedIcon from '@mui/icons-material/PermIdentityOutlined';
-import { HomeIcon, ChartBarIcon, InformationCircleIcon, UserIcon } from '@heroicons/react/outline';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import PermIdentityOutlinedIcon from "@mui/icons-material/PermIdentityOutlined";
+import {
+  HomeIcon,
+  ChartBarIcon,
+  InformationCircleIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
 
 export default function SideBar({ children }) {
-    const location = useLocation();
+  const location = useLocation();
 
-    const [active_link, set_active_link] = useState('home');
-    useEffect(() => {
-        console.log("hello")
-      const pathname = window.location.pathname.split('/')[1];
-      set_active_link(pathname);
-    }, []);
+  const [active_link, set_active_link] = useState("home");
+  useEffect(() => {
+    console.log("hello");
+    const pathname = window.location.pathname.split("/")[1];
+    set_active_link(pathname);
+  }, []);
 
-    return (
-      <div className='flex flex-row h-full w-full rounded-full'>
-        <div className='bg-darkgray rounded-r-2xl h-full'>
-            <div className=" mx-[30px] mt-[40px] sidebar w-[250px] text-white">
-            <div className='my-[5px] mb-[20px]'>
-                <img src='http://placehold.it/250x80'></img>
-            </div>
-            <nav>
-                <ul>
-                <li className='ml-[10px] mb-[15px] font-[500]'>
-                    <div className={active_link != 'home' ? 'bg-darkgray text-white py-[10px] rounded-xl' : 'bg-white text-black py-[10px] px-[10px] rounded-xl'}>
-                        <div className="flex">
-                            <HomeIcon class="h-6 w-6" />
-                            <Link to="/home" className="ml-[15px]" onClick={() => {set_active_link('home')}}>
-                            Home
-                            </Link>
-                        </div>
-                    </div>
-                </li>
-                <li className='ml-[10px] mb-[15px] font-[500]'>
-                    <div className={active_link != 'events' ? 'bg-darkgray text-white py-[10px] rounded-xl' : 'bg-white text-black py-[10px] px-[10px] rounded-xl'}>
-                        <div className="flex">
-                            <ChartBarIcon class="h-6 w-6"/>
-                            <Link to="/events" className="ml-[15px]" onClick={ () => {set_active_link('events')}}>
-                            Events
-                            </Link>
-                        </div>
-                    </div>
-                </li>
-                <li className='ml-[10px] mb-[15px] font-[500]'>
-                    <div className={active_link != 'about' ? 'bg-darkgray text-white py-[10px] rounded-xl' : 'bg-white text-black py-[10px] px-[10px] rounded-xl'}>
-                         <div className="flex">
-                            <InformationCircleIcon class="h-6 w-6" />
-                            <Link to="/about" className="ml-[15px]" onClick={ () => {set_active_link('about')}}>
-                            About
-                            </Link>
-                        </div>
-                    </div>
-                </li>
-                <li className='ml-[10px] mb-[15px] font-[500]'>
-                    <div className={active_link != 'user' ? 'bg-darkgray text-white py-[10px] rounded-xl' : 'bg-white text-black py-[10px] px-[10px] rounded-xl'}>
-                         <div className="flex">
-                            <UserIcon class="h-6 w-6" />
-                            <Link to="/user" className="ml-[15px]" onClick={ () => {set_active_link('user')}}>
-                            Volunteering
-                            </Link>
-                        </div>
-                    </div>
-                </li>
-                </ul>
-            </nav>
-            </div>
-        </div>
-        <div className='w-full h-full'>
-          {children}
+  return (
+    <div className="flex flex-row h-full w-full rounded-full">
+      <div className="bg-darkgray rounded-r-2xl h-full">
+        <div className=" mx-[30px] mt-[40px] sidebar w-[250px] text-white">
+          <div className="my-[5px] mb-[20px]">
+            <img src="http://placehold.it/250x80"></img>
+          </div>
+          <nav>
+            <ul>
+              <li className="ml-[10px] mb-[15px] font-[500]">
+                <div
+                  className={
+                    active_link != "home"
+                      ? "bg-darkgray text-white py-[10px] rounded-xl"
+                      : "bg-white text-black py-[10px] px-[10px] rounded-xl"
+                  }
+                >
+                  <div className="flex">
+                    <HomeIcon class="h-6 w-6" />
+                    <Link
+                      to="/home"
+                      className="ml-[15px]"
+                      onClick={() => {
+                        set_active_link("home");
+                      }}
+                    >
+                      Home
+                    </Link>
+                  </div>
+                </div>
+              </li>
+              <li className="ml-[10px] mb-[15px] font-[500]">
+                <div
+                  className={
+                    active_link != "events"
+                      ? "bg-darkgray text-white py-[10px] rounded-xl"
+                      : "bg-white text-black py-[10px] px-[10px] rounded-xl"
+                  }
+                >
+                  <div className="flex">
+                    <ChartBarIcon class="h-6 w-6" />
+                    <Link
+                      to="/events"
+                      className="ml-[15px]"
+                      onClick={() => {
+                        set_active_link("events");
+                      }}
+                    >
+                      Events
+                    </Link>
+                  </div>
+                </div>
+              </li>
+              <li className="ml-[10px] mb-[15px] font-[500]">
+                <div
+                  className={
+                    active_link != "about"
+                      ? "bg-darkgray text-white py-[10px] rounded-xl"
+                      : "bg-white text-black py-[10px] px-[10px] rounded-xl"
+                  }
+                >
+                  <div className="flex">
+                    <InformationCircleIcon class="h-6 w-6" />
+                    <Link
+                      to="/about"
+                      className="ml-[15px]"
+                      onClick={() => {
+                        set_active_link("about");
+                      }}
+                    >
+                      About
+                    </Link>
+                  </div>
+                </div>
+              </li>
+              <li className="ml-[10px] mb-[15px] font-[500]">
+                <div
+                  className={
+                    active_link != "user"
+                      ? "bg-darkgray text-white py-[10px] rounded-xl"
+                      : "bg-white text-black py-[10px] px-[10px] rounded-xl"
+                  }
+                >
+                  <div className="flex">
+                    <UserIcon class="h-6 w-6" />
+                    <Link
+                      to="/user"
+                      className="ml-[15px]"
+                      onClick={() => {
+                        set_active_link("user");
+                      }}
+                    >
+                      Volunteering
+                    </Link>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
-    );
-  }
+      <div className="w-full h-full">{children}</div>
+    </div>
+  );
+}
