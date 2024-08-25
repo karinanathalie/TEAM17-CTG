196751a0052b06513848163574398577ac383b94
Ananda Indra Tan <anandaindratan45@gmail.com>
2024-08-26 04:44:10 +0800

    Feedback

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -11,6 +11,7 @@ import LocationComponent from "../Components/Distance.js";
 import { ScheduleCard, CarousellCard, EventCard } from "../Components/Cards.js";
 import { ButtonAccount, ButtonFull } from "../Components/Button.js";
 import SideBar from "../Components/SideBar.js";
+import Feedbackform from "../Components/Feedbackform.js";
 import Calendarr from "../Components/Calendar.jsx";
 import Achievements from "../Components/Achievements.js";
 import { SwipingCard } from "../Components/SwipingCard.js";
@@ -129,7 +130,9 @@ export default function HomeView() {
               {/* <Calendarr /> */}
             </Wrapper>
           </Wrapper>
-          <Wrapper className="border border-black h-[240px] rounded-[8px] w-[670px]"></Wrapper>
+          <Wrapper className="border border-black h-[240px] rounded-[8px] w-[670px]">
+            <Feedbackform />
+          </Wrapper>
         </Wrapper>
       </Wrapper>
       <Wrapper>
