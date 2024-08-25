f27fca61fbdfa68efbb7581c187af701a5d26f33
Saidbek Khalikov <skhalikov2@gmail.com>
2024-08-26 00:21:37 +0800

    Fixing the merge conflicts and event related pages

--- front-end/src/Pages/EventDetails.js
+++ front-end/src/Pages/EventDetails.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { styled } from "styled-components";
 import { useHistory } from 'react-router-dom';
 import { BackButton, VolunteerParticipantToggle } from '../Components/Button';
@@ -10,11 +10,7 @@ const EventListWrapper = styled.div``;
 
 
 export default function EventDetails(){
-    const [current_role, setRole] = useState('Participant');
-
-    const handleRoleToggle = () => {
-        setRole(prevRole => prevRole === 'Participant' ? 'Volunteer' : 'Participant');
-    };
+    const [current_role, setRole] = useState('Participant');    
 
     const history = useHistory();
 
@@ -22,7 +18,7 @@ export default function EventDetails(){
         history.goBack();
     };
 
-    const list_of_events = [
+    const [list_of_events, set_list_of_events] = useState([
         {
             "event_name": "Event 1",
             "event_date": "2023-12-12",
@@ -39,7 +35,29 @@ export default function EventDetails(){
             "event_summary": "Event 2 Summary",
             "event_role": "Volunteer"
         }
-    ]
+    ]);
+
+    const handleRoleToggle = () => {
+        setRole(prevRole => prevRole === 'Participant' ? 'Volunteer' : 'Participant');
+    };
+    useEffect(() => {
+        // fetching the event details from the backend
+        let backend_base = 'http://localhost:8000/'
+        fetch(backend_base + 'api/events', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }).then(async (res) => {
+            if (res.status == 200) {
+                // if the fetch is successful update the eventDetails json object
+                set_list_of_events(await res.json().then(data => data.map(event => event.fields)));
+            } else {
+                // if the fetch is unsuccessful, display an error message
+                alert("Internal Server Error");
+            }
+        })
+    }, []);
 
     return(
         <Container className="flex w-full h-screen">
