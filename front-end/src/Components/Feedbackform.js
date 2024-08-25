baecc6929121c5952cba02aad907cf7fde168aa8
vanessalaurel <vlaurel04@gmail.com>
2024-08-26 07:55:47 +0800

    animation

--- front-end/src/Components/Feedbackform.js
+++ front-end/src/Components/Feedbackform.js
@@ -1,38 +1,43 @@
-import react, { Component } from 'react';
+import react, { Component } from "react";
 
-import { ButtonFull } from './Button';
-import StarRatingBar from './StarRating';
+import { ButtonFull } from "./Button";
+import StarRatingBar from "./StarRating";
 
 export const FeedBackForm = (event_id) => {
-    function handleSubmission(event) {
-        event.preventDefault();
-        console.log('Feedback submitted');
-    }
+  function handleSubmission(event) {
+    event.preventDefault();
+    console.log("Feedback submitted");
+  }
 
-    return(
-        <form onSubmit={handleSubmission} className='flex flex-col justify-start px-[20px] py-[10px] rounded-[16px] w-full h-full bg-pastelyellow'>
-            <div className='font-semibold'>
-                Event Feedback
+  return (
+    <form
+      onSubmit={handleSubmission}
+      className="flex flex-col justify-start px-[20px] py-[10px] rounded-[16px] w-full h-full bg-pastelyellow"
+    >
+      <div className="font-semibold">Event Feedback</div>
+      <input
+        className="border border-gray mt-[10px] px-[10px] w-full rounded-[6px] bg-white"
+        type="text"
+        placeholder="Event Name"
+      ></input>
+      <div className="flex mt-[10px] space-x-[10px]">
+        <textarea
+          className="border border-gray px-[10px] w-full h-[90px] rounded-[6px] bg-white"
+          type="text"
+          placeholder="Write a Review"
+        ></textarea>
+        <div className="flex flex-col justify-end">
+          <div className="flex flex-col font-medium">
+            <div className="font-medium">Rating:</div>
+            <div className="flex">
+              <StarRatingBar />
             </div>
-            <input className='border border-gray mt-[10px] px-[10px] w-full rounded-[6px] bg-lightgray' type='text' placeholder='Event Name'>
-            </input>
-            <div className='flex mt-[10px] space-x-[10px]'>
-                <textarea className='border border-gray px-[10px] w-full h-[90px] rounded-[6px] bg-lightgray' type='text' placeholder='Write a Review'>
-                </textarea>
-                <div className='flex flex-col justify-end'>
-                    <div className='flex flex-col font-medium'>
-                        <div className='font-medium'>
-                            Rating:
-                        </div>
-                        <div className='flex'>
-                            <StarRatingBar />
-                        </div>
-                    </div>
-                    <ButtonFull>Submit</ButtonFull>
-                </div>
-            </div>
-        </form>
-    );
+          </div>
+          <ButtonFull>Submit</ButtonFull>
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default FeedBackForm;
--- front-end/src/Components/SwipingCard.js
+++ front-end/src/Components/SwipingCard.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import { React, useState, useEffect } from "react";
 import {
   motion,
   useMotionValue,
@@ -10,45 +9,49 @@ import { useNavigate } from "react-router-dom";
 import { CarousellCard } from "./Cards";
 
 export const SwipingCard = ({ distance }) => {
-    const [list_of_events, set_list_of_events] = useState([]);
-      useEffect(() => {
-        fetch('http://0.0.0.0:8000/api/events/')
-          .then((response) => response.json())
-          .then((data) => {
-              console.log('here is data', data)
-              const eventRows = data.map((item) => ({
-                  id: item.pk,
-                  ...item.fields,  
-              }));
-              set_list_of_events(eventRows);
-          })
-          .catch((error) => console.error('Error fetching events:', error));
-    }, []);
-    
-        console.log(list_of_events);
+  const [list_of_events, set_list_of_events] = useState([]);
+  const controls = useAnimation();
 
-    const [curr_index, setIndex] = useState(0);
+  useEffect(() => {
+    fetch("http://0.0.0.0:8000/api/events/")
+      .then((response) => response.json())
+      .then((data) => {
+        const eventRows = data.map((item) => ({
+          id: item.pk,
+          ...item.fields,
+        }));
+        set_list_of_events(eventRows);
+      })
+      .catch((error) => console.error("Error fetching events:", error));
+  }, []);
 
-    const RoutetoIndividualEvent = () => {
-        console.log("RoutetoIndividualEvent");
-    };
+  const [curr_index, setIndex] = useState(0);
 
-    const SkipEvent = () => {
-        setIndex(curr_index + 1);
-    };
+  const RoutetoIndividualEvent = () => {
+    console.log("RoutetoIndividualEvent");
+  };
 
-    let current_event = list_of_events[curr_index];
+  const SkipEvent = async () => {
+    await controls.start({ opacity: 0 }); // Fade out
+    setIndex(curr_index + 1);
+    controls.set({ opacity: 1 }); // Reset opacity for the next event
+  };
 
-    // console.log("picture", current_event.event_picture);
-    return (
-        <div className="p-[34px] flex flex-col w-[450px] h-[700px] rounded-[16px] justify-center font-poppins bg-darkgray text-gray">
-        <div className="text-[28px] font-regular text-center mb-[33px]">
-            Find the most suitable events{" "}
-            <span className="text-pastelyellow"> near you! </span>
-        </div>
-        {current_event &&
-        <div className="flex justify-center">
-            <CarousellCard
+  let current_event = list_of_events[curr_index];
+
+  return (
+    <div className="p-[34px] flex flex-col w-[450px] h-[700px] rounded-[16px] justify-center font-poppins bg-darkgray text-gray">
+      <div className="text-[28px] font-regular text-center mb-[33px]">
+        Find the most suitable events{" "}
+        <span className="text-pastelyellow">near you!</span>
+      </div>
+      {current_event && (
+        <motion.div
+          className="flex justify-center"
+          animate={controls} // Use the animation controls
+          initial={{ opacity: 1 }} // Initial opacity
+        >
+          <CarousellCard
             onClickOther={SkipEvent}
             onClickRegis={RoutetoIndividualEvent}
             date={current_event.event_date}
@@ -56,9 +59,74 @@ export const SwipingCard = ({ distance }) => {
             distance="2 KM"
             eventPicture={current_event.event_image}
             eventName={current_event.event_name}
-            />
-        </div>
-        }
-        </div>
-    );
+          />
+        </motion.div>
+      )}
+    </div>
+  );
 };
+
+// import React from "react";
+// import { useState, useEffect } from "react";
+// import {
+//   motion,
+//   useMotionValue,
+//   useTransform,
+//   useAnimation,
+// } from "framer-motion";
+// import { useNavigate } from "react-router-dom";
+// import { CarousellCard } from "./Cards";
+
+// export const SwipingCard = ({ distance }) => {
+//     const [list_of_events, set_list_of_events] = useState([]);
+//       useEffect(() => {
+//         fetch('http://0.0.0.0:8000/api/events/')
+//           .then((response) => response.json())
+//           .then((data) => {
+//               console.log('here is data', data)
+//               const eventRows = data.map((item) => ({
+//                   id: item.pk,
+//                   ...item.fields,
+//               }));
+//               set_list_of_events(eventRows);
+//           })
+//           .catch((error) => console.error('Error fetching events:', error));
+//     }, []);
+
+//         console.log(list_of_events);
+
+//     const [curr_index, setIndex] = useState(0);
+
+//     const RoutetoIndividualEvent = () => {
+//         console.log("RoutetoIndividualEvent");
+//     };
+
+//     const SkipEvent = () => {
+//         setIndex(curr_index + 1);
+//     };
+
+//     let current_event = list_of_events[curr_index];
+
+//     // console.log("picture", current_event.event_picture);
+//     return (
+//         <div className="p-[34px] flex flex-col w-[450px] h-[700px] rounded-[16px] justify-center font-poppins bg-darkgray text-gray">
+//         <div className="text-[28px] font-regular text-center mb-[33px]">
+//             Find the most suitable events{" "}
+//             <span className="text-pastelyellow"> near you! </span>
+//         </div>
+//         {current_event &&
+//         <div className="flex justify-center">
+//             <CarousellCard
+//             onClickOther={SkipEvent}
+//             onClickRegis={RoutetoIndividualEvent}
+//             date={current_event.event_date}
+//             location={current_event.event_location}
+//             distance="2 KM"
+//             eventPicture={current_event.event_image}
+//             eventName={current_event.event_name}
+//             />
+//         </div>
+//         }
+//         </div>
+//     );
+// };
