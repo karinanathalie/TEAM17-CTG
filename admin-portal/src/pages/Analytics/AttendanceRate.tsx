3a72cc017ebca8fad3f94ef92235ec0210e754c5
Karina Nathalie <karinanathalie2@gmail.com>
2024-08-26 03:58:22 +0800

    add analytics

--- admin-portal/src/pages/Analytics/AttendanceRate.tsx
+++ admin-portal/src/pages/Analytics/AttendanceRate.tsx
@@ -2,61 +2,96 @@ import React, { useState, useEffect } from 'react';
 import { PieChart } from '@mui/x-charts/PieChart';
 import EventDropdown from '../../helpers/EventDropdown';
 import { Typography, Box, Paper } from '@mui/material';
+import { BarPlot, ChartContainer } from '@mui/x-charts';
 
 const AttendanceRate: React.FC = () => {
     const [attendanceData, setAttendanceData] = useState<any[]>([]);
+    const [demographicData, setDemographicData] = useState<any[]>([]);
     const [selectedEvent, setSelectedEvent] = useState<any>(null);
 
     useEffect(() => {
         fetch('http://0.0.0.0:8000/api/analytics/attendance')
-          .then((response) => response.json())
-          .then((data) => {
-              const rows = data.map((item: any) => ({
-                  id: item.pk,
-                  ...item.fields,  
-              }));
-              console.log('row', data, rows)
-              setAttendanceData(rows);
-          })
-          .catch((error) => console.error('Error fetching data:', error));
+            .then((response) => response.json())
+            .then((data) => {
+                setAttendanceData(data.data);
+            })
+            .catch((error) => console.error('Error fetching events:', error));
     }, []);
-    console.log('data', attendanceData)
+
+    useEffect(() => {
+        fetch('http://0.0.0.0:8000/api/analytics/demographic')
+            .then((response) => response.json())
+            .then((data) => {
+                setDemographicData(data.data);
+            })
+            .catch((error) => console.error('Error fetching events:', error));
+    }, []);
 
     const handleEventSelect = (event: any) => {
         if (event) {
-            const eventDetails = attendanceData.find((e) => e.event_name === event.title);
-            setSelectedEvent(eventDetails);
+            const eventAttendance = attendanceData?.find((e) => e.event_name === event.title);
+            const eventDemographics = demographicData?.find((e) => e?.event_name === event.title);
+            setSelectedEvent(eventAttendance);
+            setDemographicData([eventDemographics]);
         } else {
             setSelectedEvent(null);
         }
     };
 
     return (
-        <Paper elevation={3} sx={{ padding: '20px', margin: 8, width: '400px' }}>
-        <Typography variant="h6" gutterBottom>
-          Attendance Rate
-        </Typography>
-        
-        <Box sx={{marginTop: 4}}>
-        <EventDropdown onEventSelect={handleEventSelect}/>
+        <>
+        <Paper elevation={3} sx={{ padding: '20px', marginTop: 8, marginLeft: 8, width: '1200px' }}>
+            <Typography variant="h5" gutterBottom>
+                User Distribution 
+            </Typography>
+
+            <Box sx={{ marginTop: 4 }}>
+                <EventDropdown onEventSelect={handleEventSelect} />
+            </Box>
+            {selectedEvent && selectedEvent['data'].length > 0 &&
+                <Paper key={selectedEvent['data'].event_id} elevation={3} sx={{ padding: 3, marginTop: 4 }}>
+            <Typography variant="h6" gutterBottom>
+                Attendance Analysis
+            </Typography>
+                <Box mt={3} display="flex" justifyContent="center">
+                    <PieChart
+                        width={400}
+                        height={300}
+                        series={[
+                            {
+                                data: selectedEvent['data'],
+                            },
+                        ]} />
+                </Box>
+                </Paper>
+            }
+
+        <Box>
+            {demographicData && demographicData.map((eventData) => {
+            const xLabels = eventData?.xLabels;
+            const yData = eventData?.yData;
+
+            return (
+                xLabels && xLabels.length > 0 && (
+                <Paper key={eventData.event_id} elevation={3} sx={{ padding: 3, marginTop: 4 }}>
+                    <Typography variant="h5" gutterBottom>
+                        Demographic Analysis
+                    </Typography>
+                    <ChartContainer
+                        width={500}
+                        height={300}
+                        series={[{ data: yData, label: 'Demographics', type: 'bar' }]}
+                        xAxis={[{ scaleType: 'band', data: xLabels }]}
+                    >
+                        <BarPlot />
+                    </ChartContainer>
+                </Paper>
+                )
+            );
+        })}
         </Box>
-  
-        <Box mt={3} display="flex" justifyContent="center">
-          <PieChart
-            width={400}
-            height={300}
-            series={[
-              {
-                data: attendanceData.map((entry: any) => ({
-                  id: entry.id,
-                  value: entry.value,
-                  label: entry.label,
-                })),
-              },
-            ]}
-          />
-        </Box>
-      </Paper>
+            </Paper>
+            </>
     );
 };
 
